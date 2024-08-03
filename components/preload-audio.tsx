5c6e892d5b79f3a7c003f241198575fbb7ff9313
Sax Dhiut <mail@iyxeyl.me>
2024-08-03 22:44:16 +0400

    refactor: remove unnecessary event listeners
    Removed the canplaythrough event listeners and associated logic to streamline audio preloading. This reduces complexity and removes redundant code from the preload-audio component.

--- components/preload-audio.tsx
+++ components/preload-audio.tsx
@@ -9,29 +9,11 @@ type PreloadAudioProps = {
 
 export const PreloadAudio = ({ trackAudios }: PreloadAudioProps) => {
 	useEffect(() => {
-		const audios: {
-			audio: HTMLAudioElement
-			onLoad: () => void
-		}[] = []
-
 		for (const trackAudio of trackAudios) {
 			for (const link of Object.values(trackAudio.audioLinks)) {
 				const audio = new Audio(link)
 				audio.preload = "auto"
 				audio.load()
-
-				const onLoad = () => {
-					console.log(`${link} is loaded and can play through.`)
-				}
-
-				audio.addEventListener("canplaythrough", onLoad)
-				audios.push({ audio, onLoad })
-			}
-		}
-
-		return () => {
-			for (const { audio, onLoad } of audios) {
-				audio.removeEventListener("canplaythrough", onLoad)
 			}
 		}
 	}, [trackAudios])
